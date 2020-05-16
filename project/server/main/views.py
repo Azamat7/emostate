e499e968a669fcafd3d2d68aa128bae950158ef8
Azamat Smagulov <s.a.smagulov@gmail.com>
2020-05-16 19:36:05 +0600

    add links

--- project/server/main/views.py
+++ project/server/main/views.py
@@ -10,10 +10,14 @@ main_blueprint = Blueprint("main", __name__,)
 
 
 @main_blueprint.route('/', methods=["GET"])
-def index():
-  return render_template('index.html')
+def outdoor_activity():
+  return render_template('outdoor_activity.html')
 
 
-@main_blueprint.route('/test', methods=['GET'])
-def test():
-  return render_template('test.html', nums=get_nums())
+@main_blueprint.route('/app_usage', methods=['GET'])
+def app_usage():
+  return render_template('app_usage.html')
+
+@main_blueprint.route('/recommendation', methods=['GET'])
+def recommendation():
+  return render_template('recommendation.html')
